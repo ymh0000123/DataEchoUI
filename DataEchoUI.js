3c087ad6724b222e4e48d49287e854a16a7f0b4d
没用的小废鼠 <107793048+ymh0000123@users.noreply.github.com>
2024-03-05 21:44:45 +0800

    1

--- DataEchoUI.js
+++ DataEchoUI.js
@@ -1,10 +1,52 @@
-// JavaScript用于添加点击事件监听器，改变点击的链接样式
 document.addEventListener("DOMContentLoaded", function() {
-    var links = document.querySelectorAll('#nav a'); // 获取所有导航链接
+    var links = document.querySelectorAll('#DataEchoUI-navbar-left a');
+
+    function removeHighlight() {
+        links.forEach(link => link.classList.remove('DataEchoUI-highlight-2'));
+    }
+
+    function highlightLinkByHash() {
+        var currentHash = window.location.hash;
+        if (currentHash) {
+            var activeLink = document.querySelector(`#DataEchoUI-navbar-left a[href="${currentHash}"]`);
+            if (activeLink) {
+                removeHighlight();
+                activeLink.classList.add('DataEchoUI-highlight-2');
+            }
+        }
+    }
+
+    highlightLinkByHash();
+
     links.forEach(function(link) {
         link.addEventListener('click', function() {
-            links.forEach(l => l.classList.remove('highlight-1')); // 移除所有链接的高亮样式
-            this.classList.add('highlight-1'); // 为点击的链接添加高亮样式
+            removeHighlight();
+            this.classList.add('DataEchoUI-highlight-2');
         });
     });
+
+    window.addEventListener('hashchange', highlightLinkByHash);
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+var links = document.querySelectorAll('#DataEchoUI-navbar-left a');
+var baseDelay = 0.2;
+
+    links.forEach(function(link, index) {
+    var delay = baseDelay * index + 's';
+    link.style.animationDelay = delay;
+    });
+});
+
+function adjustMarginTop() {
+    var navbarHeight = document.querySelector('.DataEchoUI-navbar').offsetHeight;
+    document.querySelector('#DataEchoUI-navbar-left').style.marginTop = navbarHeight + 'px';
+}
+
+function adjustMarginTop2() {
+    var navbarHeight2 = document.querySelector('.DataEchoUI-navbar').offsetHeight;
+    document.querySelector('#DataEchoUI-navbar-left-31').style.marginTop = navbarHeight2 + 'px';
+}
+
+window.onload = adjustMarginTop,adjustMarginTop2;
+window.onresize = adjustMarginTop,adjustMarginTop2;
